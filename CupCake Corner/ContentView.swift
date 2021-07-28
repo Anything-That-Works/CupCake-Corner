8bb63acb7e9dc3b66f7e0b170680cd86d1d0b617
Promal B <promal.barua.cse@gmail.com>
2021-07-28 07:09:02 +0600

    Initial Commit

---
+++ CupCake Corner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupCake Corner
+//
+//  Created by Promal on 28/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
